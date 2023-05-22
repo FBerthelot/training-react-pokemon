d5e3f5850142e3adfe7afa4e3462b51972c685c7
FBerthelot <florent.berthelot35@gmail.com>
2023-05-22 11:36:36 +0200

    TP3

--- app.js
+++
@@ -1,5 +0,0 @@
-const domContainer = document.querySelector('#my-react-application');
-
-
-const root = ReactDOM.createRoot(domContainer);
-root.render(React.createElement('h1', {}, 'Votre formateur dévoué'));
---
+++ src/App.js
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+      <header className="App-header">
+        Coucou, c'est le formateur
+      </header>
+  );
+}
+
+
