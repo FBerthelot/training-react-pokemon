560d8673532d00ffa2f6221316c0f2d5604a21c5
FBerthelot <florent.berthelot35@gmail.com>
2023-05-22 10:26:24 +0200

    TP2

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('coucou')
+const domContainer = document.querySelector('#my-react-application');
+
+
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement('h1', {}, 'Votre formateur dévoué'));
