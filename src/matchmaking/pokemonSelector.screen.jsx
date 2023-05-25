9b30dd10f3f5229e85d9c026f9d6fccfa9afaf76
FBerthelot <florent.berthelot35@gmail.com>
2023-05-25 22:07:47 +0200

    TP14

--- src/matchmaking/pokemonSelector.screen.jsx
+++ src/matchmaking/pokemonSelector.screen.jsx
@@ -1,13 +1,43 @@
-import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form";
+import classes from './pokemonSelector.module.css'
+import { useNavigate } from "react-router-dom";
 
 export const PokemonSelectorScreen = () => {
+    const { register, handleSubmit } = useForm();
+    const navigate = useNavigate()
+
+    const pokemonList = [
+        'charmander',
+        'pikachu',
+        'bulbasaur'
+    ]
+    
+    const onSubmit = handleSubmit(({attacker, defender}) => {
+        navigate(`/battle/${attacker}/${defender}`)
+    })
+
     return <main>
         <h1>Selectionne le combat que tu veux voir</h1>
 
-        <ul>
-            <li><Link to="/battle/pikachu/charmander">Pikachu Vs Salamèche</Link></li>
-            <li><Link to="/battle/charmander/pikachu">Salamèche Vs Pikachu</Link></li>
-            <li><Link to="/battle/pikachu/bulbasaur">Pikachu Vs Bulbizzare</Link></li>
-        </ul>
+        <form className={classes.matchmaking_form} onSubmit={onSubmit}>
+
+            <label>
+                Attacker:
+                <select {...register('attacker')}>
+                    {pokemonList.map(name => <option key={name} value={name}>{name}</option>)}
+                </select>
+            </label>
+
+
+            <label>
+                Defender:
+                <select {...register('defender')}>
+                    {pokemonList.map(name => <option key={name} value={name}>{name}</option>)}
+                </select>
+            </label>
+
+            <button className={classes.matchmaking_button}  type="submit">Fight!</button>
+
+        </form>
     </main>
 }
--- src/matchmaking/pokemonSelector.screen.test.jsx
+++ src/matchmaking/pokemonSelector.screen.test.jsx
@@ -1,11 +1,45 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { PokemonSelectorScreen } from "./pokemonSelector.screen"
 import { BrowserRouter } from "react-router-dom";
+import userEvent from "@testing-library/user-event";
+import {navigateMock} from 'react-router-dom'
+
+jest.mock('react-router-dom', () => {
+    const navigateMock = jest.fn()
+    return {
+        ...jest.requireActual('react-router-dom'),
+        useNavigate: () => navigateMock,
+        navigateMock
+    }
+});
+
 
 
 describe('pokemonSelector screen', () => {
     // There is no valuable test to make here, this component just display links...
-    it('should not throw error', () => {
+    it('should navigate to the battle between pika and sala page when select pika and sala and submit the form', async () => {
         render(<PokemonSelectorScreen />, {wrapper: BrowserRouter});
+
+        const user = userEvent.setup()
+
+        await user.selectOptions(screen.getByLabelText(/Attacker/), 'pikachu')
+        await user.selectOptions(screen.getByLabelText(/Defender/), 'charmander')
+
+        await user.click(screen.getByRole('button'))
+
+        expect(navigateMock).toHaveBeenCalledWith('/battle/pikachu/charmander')
+    })
+
+    it('should navigate to the battle between bulbi and pika page when select bulbi and pika and submit the form', async () => {
+        render(<PokemonSelectorScreen />, {wrapper: BrowserRouter});
+
+        const user = userEvent.setup()
+
+        await user.selectOptions(screen.getByLabelText(/Attacker/), 'bulbasaur')
+        await user.selectOptions(screen.getByLabelText(/Defender/), 'pikachu')
+
+        await user.click(screen.getByRole('button'))
+
+        expect(navigateMock).toHaveBeenCalledWith('/battle/bulbasaur/pikachu')
     })
 })
